5108f127e0cfbbabe3e1f3237d3ab1385cbbc025
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2022-07-29 20:39:57 +0200

    Adding extra function headers to be implemented

--- matrices/MxAlg.h
+++ matrices/MxAlg.h
@@ -36,6 +36,7 @@ void printMatrix(Matrix* matrix);
 /*!
  * @param mat1 pointer to matrix to add
  * @param mat2 pointer to matrix to add 
+ * @brief mat1 and mat2 must be of same size
  * @return Matrix addition of mat1 + mat2
 */
 Matrix* addMat(Matrix* mat1, Matrix* mat2);
@@ -43,9 +44,29 @@ Matrix* addMat(Matrix* mat1, Matrix* mat2);
 /*!
  * @param mat1 pointer to matrix to subtract from
  * @param mat2 pointer to matrix to subtract
+ * @brief mat1 and mat2 must be of same size
  * @return mat1 - mat2
- * @
 */
 Matrix* subMat(Matrix* mat1, Matrix* mat2);
+
+/*!
+ * @param mat1 pointer to matrix to be multiplied
+ * @param mat2 pointer to matrix to be multiplied
+ * @brief if mat1 is of size n*m, then mat2 must be of size m*y, where n and y can be any numbers, but inner dimensions must equal
+ * @return multiplication of mat1*mat2
+*/
+Matrix* mulMat(Matrix* mat1, Matrix* mat2);
+
+/*!
+ * @param matrix pointer to matrix that we need to transpose
+ * @return the transpose of the input matrix
+*/
+Matrix* transMat(Matrix* matrix);
+
+/*!
+ * @param matrix pointer to matrix to which we want to calculate the determinant
+ * @return The determinant of the input matrix
+*/
+double detMat(Matrix* matrix);
 
 #endif // MXALG_H
